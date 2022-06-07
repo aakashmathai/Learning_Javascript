dc4da9a28539d2e3451e138dbd576eaa730fd942
aakashmathai <aakashmathai@gmail.com>
2022-06-06 18:49:07 -0700

    Completed Passage Counter

--- PassengerCounter/index.js
+++ PassengerCounter/index.js
@@ -1,8 +1,21 @@
 //document.getElementById("count").innerText = 7
 let countEl = document.getElementById("count")
+let savedCountsP = document.getElementById("saved_counts_p")
 let count = 0
+
 function increment() {
 
-    count = count + 1
+    count += 1
     countEl.innerText = count
+}
+
+function save() {
+    let text = savedCountsP.textContent
+    if (text === "") {
+        savedCountsP.textContent = count
+    } else {
+        savedCountsP.textContent = text + " + " + count
+    }
+    count = 0
+    countEl.textContent = count
 }
---
+++ Readme.md
@@ -0,0 +1,4 @@
+This is me trying to learn Javascipt
+
+Videos Watched:
+[JavaScript Programming - Full Course](https://youtu.be/jS4aFq5-91M)
